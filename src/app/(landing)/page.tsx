fcba575b1b62600c6e819a44a9166e99ac137520
Umar qureshi <m.umarqureshi091@gmail.com>
2024-10-26 21:17:24 +0500

    landing page done

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import CallToAction from './_components/call-to-action'
+import DashboardSnippet from './_components/dashboard-snippet'
+import dynamic from 'next/dynamic'
+
+const PricingSection = dynamic(
+  ()=> import('./_components/pricing')
+  .then((component) =>component.PricingSection,)
+  ,{ssr:true}
+)
+
 
 function HomePage() {
   return (
+    <main className='md:px-10 py-20 flex flex-col gap-36'>
     <div>
       <CallToAction/>
+      <DashboardSnippet />
+      <PricingSection />
     </div>
+    </main>
   )
 }
 
