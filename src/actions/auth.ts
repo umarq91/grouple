222b852285bae807ae29bf17f9b23457f49ea0e3
Umar qureshi <m.umarqureshi091@gmail.com>
2024-11-02 19:55:14 +0500

    sign-in page UI

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,14 +1,36 @@
 "use server"
 
+import { client } from "@/lib/prisma"
 import { currentUser } from "@clerk/nextjs/server"
 
-export const onAuthenticatedUser = async ()=>{
+export const onAuthenticatedUser = async () => {
     try {
-            const clerk  = await currentUser();
-            if(!clerk) return {status:404}
+        const clerk = await currentUser()
+        if (!clerk) return { status: 404 }
 
-            
+        const user = await client.user.findUnique({
+            where: {
+                clerkId: clerk.id,
+            },
+            select: {
+                id: true,
+                firstname: true,
+                lastname: true,
+            },
+        })
+
+        if (user) {
+            return {
+                status: 200,
+                id: user.id,
+                image: clerk.imageUrl,
+                username: `${user.firstname} ${user.lastname}`,
+            }
+            return { status: 404 }
+        }
     } catch (error) {
-        
+        return {
+            status: 400,
+        }
     }
 }
